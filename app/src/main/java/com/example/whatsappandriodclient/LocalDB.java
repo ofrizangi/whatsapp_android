19b12be6ecfed64f0ce4a3078cf2f5a92458a014
Ofri Zangi <ofrizangi@gmail.com>
2022-06-21 00:29:07 +0300

    update

--- app/src/main/java/com/example/whatsappandriodclient/LocalDB.java
+++ app/src/main/java/com/example/whatsappandriodclient/LocalDB.java
@@ -40,7 +40,7 @@ public abstract class LocalDB extends RoomDatabase {
             synchronized (LocalDB.class) {
                 if (INSTANCE == null) {
                     INSTANCE = databaseBuilder(context,
-                                    LocalDB.class, "million9")
+                                    LocalDB.class, "mywhatsappDB")
                             .allowMainThreadQueries()
                             .build();
                 }
